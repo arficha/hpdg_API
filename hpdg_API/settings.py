e242f6263bdfe92360dab826d092e392b3194760
Rogg99 <roggone99@gmail.com>
2024-07-22 04:31:49 +0100

    MAJ

--- hpdg_API/settings.py
+++ hpdg_API/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-sxvf7n6#x!r0^b00@0#qib#@zu0tor@rgdn+)g-=1qu^-onc*@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
